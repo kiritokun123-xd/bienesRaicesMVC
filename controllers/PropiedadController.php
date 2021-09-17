dcdfb1a706abf40acb0f9b30d18233f8c875ebd3
kiritokun123-xd <david22android@gmail.com>
2021-09-17 16:54:55 -0500

    guardado

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -28,35 +28,35 @@ class PropiedadController{
         $errores = Propiedad::getErrores();
 
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
-            /*CREA UNA NUEVA INSTANCIA*/
-        $propiedad = new Propiedad($_POST['propiedad']);
+                /*CREA UNA NUEVA INSTANCIA*/
+            $propiedad = new Propiedad($_POST['propiedad']);
 
-        //GENERAR UN NOMBRE UNICO
-        $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
+            //GENERAR UN NOMBRE UNICO
+            $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
 
-        //SETEAR LA IMAGEN
-        //realiza un RESIZE A LA IMAGEN CON INTERVENTION
-        if($_FILES['propiedad']['tmp_name']['imagen']){
-            $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800,600);
-            $propiedad->setImagen($nombreImagen);
-        }
-
-        /*VALIDAR*/
-        $errores = $propiedad->validar();
-
-        //REVISAR QUE EL ARREGLO DE ERRORES ESTE VACIO
-        if(empty($errores)){
-            //crear la carpeta imagenes
-            if(!is_dir(CARPETA_IMAGENES)){
-                mkdir(CARPETA_IMAGENES);
+            //SETEAR LA IMAGEN
+            //realiza un RESIZE A LA IMAGEN CON INTERVENTION
+            if($_FILES['propiedad']['tmp_name']['imagen']){
+                $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800,600);
+                $propiedad->setImagen($nombreImagen);
             }
 
-            //GUARDA LA IMAGEN EN EL SERVIDOR
-            $image->save(CARPETA_IMAGENES . $nombreImagen);
+            /*VALIDAR*/
+            $errores = $propiedad->validar();
 
-            //SUBE A LA BD
-            $propiedad->guardar();
-        }
+            //REVISAR QUE EL ARREGLO DE ERRORES ESTE VACIO
+            if(empty($errores)){
+                //crear la carpeta imagenes
+                if(!is_dir(CARPETA_IMAGENES)){
+                    mkdir(CARPETA_IMAGENES);
+                }
+
+                //GUARDA LA IMAGEN EN EL SERVIDOR
+                $image->save(CARPETA_IMAGENES . $nombreImagen);
+
+                //SUBE A LA BD
+                $propiedad->guardar();
+            }
         }
 
         $router->render('propiedades/crear',[
@@ -75,6 +75,36 @@ class PropiedadController{
         $vendedores = Vendedor::all();
 
         $errores = Propiedad::getErrores();
+
+        //EJECUTAR EL CODIGO DESPUES DE QuE EL USUARIO ENVIA EL FORMULARIO
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            //Asignar los atributos
+            $args = $_POST['propiedad'];
+            
+            $propiedad->sincronizar($args);
+            
+            $errores = $propiedad->validar();
+            
+            //Validación subida de archivos
+            //GENERAR UN NOMBRE UNICO
+            $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
+            
+            if($_FILES['propiedad']['tmp_name']['imagen']){
+                $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800,600);
+                $propiedad->setImagen($nombreImagen);
+            }
+
+            //REVISAR QUE EL AAREGLO DE ERRORES ESTE VACIO
+            if(empty($errores)){
+                if($_FILES['propiedad']['tmp_name']['imagen']){
+                    //GUARDA LA IMAGEN EN EL SERVIDOR
+                    $image->save(CARPETA_IMAGENES . $nombreImagen);
+                }
+                
+                $propiedad->guardar();
+            }
+
+        }
 
         $router->render('/propiedades/actualizar',[
             'propiedad' => $propiedad,

d9acde4684ff3d21d8f08191254b262d47f85a95
kiritokun123-xd <david22android@gmail.com>
2021-09-20 16:30:30 -0500

    guardado

---
+++ controllers/PaginasController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Controllers;
+
+class PaginasController{
+    public static function index(){
+        echo "desdeindex";
+    }
+    public static function nosotros(){
+        echo "desde nosotros";
+    }
+    public static function propiedades(){
+        echo "desde propiedades";
+    }
+    public static function propiedad(){
+        echo "desde propiedad";
+    }
+    public static function blog(){
+        echo "desde blog";
+    }
+    public static function entrada(){
+        echo "desde entrada";
+    }
+    public static function contacto(){
+        echo "desde contacto";
+    }
+}

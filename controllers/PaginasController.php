0cb1066db5d917d41cb26457c0e90de61d9c4a64
kiritokun123-xd <david22android@gmail.com>
2021-09-21 21:19:07 -0500

    GUARDADO

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -2,26 +2,50 @@
 
 namespace Controllers;
 
+use Model\Propiedad;
+use MVC\Router;
+
 class PaginasController{
-    public static function index(){
-        echo "desdeindex";
+    public static function index(Router $router){
+        $propiedades = Propiedad::get(3);
+        $inicio = true;
+
+        $router->render('paginas/index',[
+            'propiedades' => $propiedades,
+            'inicio' => $inicio
+        ]);
     }
-    public static function nosotros(){
-        echo "desde nosotros";
+    public static function nosotros(Router $router){
+        $router->render('paginas/nosotros',[
+        ]);
     }
-    public static function propiedades(){
-        echo "desde propiedades";
+    public static function propiedades(Router $router){
+        $propiedades = Propiedad::all();
+
+        $router->render('paginas/propiedades',[
+            'propiedades' => $propiedades
+        ]);
     }
-    public static function propiedad(){
-        echo "desde propiedad";
+    public static function propiedad(Router $router){
+        $id = validarORedireccionar('/propiedades');
+
+        $propiedad = Propiedad::find($id); 
+
+        $router->render('paginas/propiedad',[
+            'propiedad' => $propiedad
+        ]);
     }
-    public static function blog(){
-        echo "desde blog";
+    public static function blog(Router $router){
+
+        $router->render('paginas/blog',[
+        ]);
     }
-    public static function entrada(){
-        echo "desde entrada";
+    public static function entrada(Router $router){
+        $router->render('paginas/entrada',[
+        ]);
     }
-    public static function contacto(){
-        echo "desde contacto";
+    public static function contacto(Router $router){
+        $router->render('paginas/contacto',[
+        ]);
     }
 }
--- public/index.php
+++ public/index.php
@@ -26,7 +26,7 @@ $router->post('/vendedores/eliminar', [VendedorController::class,'eliminar']);
 
 //Zona pública
 $router->get('/',[PaginasController::class, 'index']);
-$router->get('/nostros',[PaginasController::class, 'nosotros']);
+$router->get('/nosotros',[PaginasController::class, 'nosotros']);
 $router->get('/propiedades',[PaginasController::class, 'propiedades']);
 $router->get('/propiedad',[PaginasController::class, 'propiedad']);
 $router->get('/blog',[PaginasController::class, 'blog']);
